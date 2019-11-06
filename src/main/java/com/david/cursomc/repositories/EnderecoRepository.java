904cb6d74cde9d212e350c964a7377a086902c98
JOSEDAVIDJUNIOR47 <jdavid_junior@bol.com.br>
2019-11-05 21:18:26 -0300

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/com/david/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.david.cursomc.repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.david.cursomc.domain.Cliente;
+import com.david.cursomc.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+
+}

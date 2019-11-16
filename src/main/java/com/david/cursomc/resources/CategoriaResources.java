cd7d98a98aa595f262b3b601a077246602b42d0c
JOSEDAVIDJUNIOR47 <jdavid_junior@bol.com.br>
2019-11-16 20:23:44 -0300

    paginação com parametros opcionais na requisição

--- src/main/java/com/david/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/david/cursomc/resources/CategoriaResources.java
@@ -5,11 +5,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -56,6 +59,26 @@ public class CategoriaResources {
 	public ResponseEntity<List<CategoriaDTO>> findAll() {
 		List<Categoria> list = service.findAll();
 		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@RequestMapping(value="pageASC",method = RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPageAsc(
+		@RequestParam(value="page",defaultValue = "0") Integer page,
+		@RequestParam(value="linesPerPage",defaultValue = "24") Integer linesPerPage,
+		@RequestParam(value="orderBy",defaultValue = "nome") String orderBy){
+		Page<Categoria> list = service.findPage(page, linesPerPage, Sort.by(orderBy).ascending());
+		Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj));
+		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@RequestMapping(value="pageDesc",method = RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPageDesc(
+		@RequestParam(value="page",defaultValue = "0") Integer page,
+		@RequestParam(value="linesPerPage",defaultValue = "24") Integer linesPerPage,
+		@RequestParam(value="orderBy",defaultValue = "nome") String orderBy){
+		Page<Categoria> list = service.findPage(page, linesPerPage, Sort.by(orderBy).descending());
+		Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj));
 		return ResponseEntity.ok().body(listDto);
 	}
 	
--- src/main/java/com/david/cursomc/services/CategoriaService.java
+++ src/main/java/com/david/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.david.cursomc.domain.Categoria;
@@ -45,5 +48,9 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll(){
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage,Sort sort) {		
+		return repo.findAll(PageRequest.of(page,linesPerPage, sort));
 	}
 }

0f66aab602b8f1da45919fd59742bf5a16caa1f6
JOSEDAVIDJUNIOR47 <jdavid_junior@bol.com.br>
2019-11-26 16:51:47 -0300

    Validacao customizada de CPF ou CNPJ na insercao de Cliente

--- src/main/java/com/david/cursomc/resources/exception/FieldMessage.java
+++ src/main/java/com/david/cursomc/resources/exception/FieldMessage.java
@@ -2,23 +2,23 @@ package com.david.cursomc.resources.exception;
 
 import java.io.Serializable;
 
-public class FildMessage implements Serializable {	
+public class FieldMessage implements Serializable {	
 	private static final long serialVersionUID = 1L;
 	
 	private String fielName;
 	private String message;
 	
-	public FildMessage() {
+	public FieldMessage() {
 		
 	}
 	
-	public FildMessage(String fielName, String message) {
+	public FieldMessage(String fielName, String message) {
 		super();
 		this.fielName = fielName;
 		this.message = message;
 	}
 
-	public String getFielName() {
+	public String getFieldName() {
 		return fielName;
 	}
 	public void setFielName(String fielName) {

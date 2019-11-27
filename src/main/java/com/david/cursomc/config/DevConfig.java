bf5ec8e1d3b2732a8a421a2c407f9f972a280ed4
JOSEDAVIDJUNIOR47 <jdavid_junior@bol.com.br>
2019-11-27 17:26:27 -0300

    Criando profile de desenvolvimento

---
+++ src/main/java/com/david/cursomc/config/DevConfig.java
@@ -0,0 +1,33 @@
+package com.david.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.david.cursomc.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value(value = "spring.jpa.hibernate.ddl-auto")
+	private String Strategy;
+			
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		if(!"create".equals(Strategy))
+			return false;
+		
+		dbService.instantiateTestDataBase();
+		return true;
+	}
+	
+
+}

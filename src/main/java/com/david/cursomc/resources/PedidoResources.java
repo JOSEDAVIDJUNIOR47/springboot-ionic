886edcb0a6569838757b1fdef0d584a56569ca32
JOSEDAVIDJUNIOR47 <jdavid_junior@bol.com.br>
2020-03-30 11:25:55 -0300

    Cliente so recupera seus pedidos

--- src/main/java/com/david/cursomc/resources/PedidoResources.java
+++ src/main/java/com/david/cursomc/resources/PedidoResources.java
@@ -5,15 +5,20 @@ import java.net.URI;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.david.cursomc.domain.Categoria;
 import com.david.cursomc.domain.Pedido;
+import com.david.cursomc.dto.CategoriaDTO;
 import com.david.cursomc.services.PedidoService;
 
 @RestController
@@ -37,6 +42,16 @@ public class PedidoResources {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
 				.path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value="pageDesc",method = RequestMethod.GET)
+	public ResponseEntity<Page<Pedido>> findPageDesc(
+		@RequestParam(value="page",defaultValue = "0") Integer page,
+		@RequestParam(value="linesPerPage",defaultValue = "24") Integer linesPerPage,
+		@RequestParam(value="orderBy",defaultValue = "instante") String orderBy){
+		Page<Pedido> list = service.findPage(page, linesPerPage, orderBy);
+		
+		return ResponseEntity.ok().body(list);
 	}
 
 }
--- src/main/java/com/david/cursomc/services/PedidoService.java
+++ src/main/java/com/david/cursomc/services/PedidoService.java
@@ -3,10 +3,18 @@ package com.david.cursomc.services;
 import java.util.Date;
 import java.util.Optional;
 
+import javax.print.attribute.standard.PageRanges;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.david.cursomc.domain.Categoria;
+import com.david.cursomc.domain.Cliente;
 import com.david.cursomc.domain.ItemPedido;
 import com.david.cursomc.domain.PagamentoComBoleto;
 import com.david.cursomc.domain.Pedido;
@@ -14,6 +22,9 @@ import com.david.cursomc.domain.enums.EstadoPagamento;
 import com.david.cursomc.repositories.ItemPedidoRepository;
 import com.david.cursomc.repositories.PagamentoRepository;
 import com.david.cursomc.repositories.PedidoRepository;
+import com.david.cursomc.security.UserSS;
+import com.david.cursomc.security.UserService;
+import com.david.cursomc.services.exceptions.AuthorizationException;
 import com.david.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -69,6 +80,16 @@ public class PedidoService {
 		emailService.sendOrderConfirmationHtmlEmail(obj);		
 		return obj;
 		
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage,String ordenaly) {	
+		UserSS user = UserService.authenticated();
+		if(user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.by(ordenaly).descending());
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 	
 
